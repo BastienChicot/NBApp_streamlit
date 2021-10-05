79c2f5aced71185c54d32ff35e353ceb73c9b0ea
BastienChicot <88327915+BastienChicot@users.noreply.github.com>
2021-10-05 21:50:02 +0200

    test avec beautiful soup

--- Fonctions.py
+++ Fonctions.py
@@ -30,7 +30,7 @@ def onglet_stat():
     
     st.title("Etudier les statistiques d'un joueur")
     col1, col2 = st.columns(2)
-    a=col1.text_input("Sélectionner un joueur")
+    a=col1.selectbox("Sélectionner un joueur", list_name)
     tm_option=col1.selectbox('Player team',liste_team)
     opp_option=col1.selectbox('Opponent team',liste_team)
     button_stats = col1.button("Afficher les stats du joueur")
@@ -47,23 +47,23 @@ def onglet_stat():
             df2=df2.rename(columns={0:"mesure"})
             graph=alt.Chart(df2).transform_calculate().mark_line().encode(
                 x="year",y=alt.Y("mesure",title="Mesures"),color="level_1")
-            #last_twenty,splits = stat_20matchs_splits(str(a))
-            against = stat_Opp_team(a,opp_option)
-            #team_rank=stat_teams(tm_option,opp_option)
+            last_twenty,splits = stat_20matchs_splits(str(a))
+            against = stat_Opp_team(str(a),str(opp_option))
+            team_rank=stat_teams(str(tm_option),str(opp_option))
             col2.dataframe(name)
             st.header("Statistiques cumulées au cours de la carrière")
             st.text("DEF = contres + interception \nMIS = fautes + balles perdues" + "\n" + 
                     "MP = minutes \nPRP = points + rebonds + passes décisives")
             st.altair_chart(graph, use_container_width=True)
             st.header("Statistiques lors des 20 derniers matchs")
-            #st.dataframe(last_twenty)
+            st.dataframe(last_twenty)
             st.header("Statistiques splits")
-            #st.dataframe(splits)
+            st.dataframe(splits)
             st.header("Statistiques contre "+opp_option)
             st.text("Les différences sont calculées par rapport à la moyenne sur la saison au cours de \nlaquelle s'est déroulé le match")
             st.dataframe(against)
             st.header("Rankings des deux équipes")
-            #st.dataframe(team_rank)
+            st.dataframe(team_rank)
         except:
             pass
     if button_stats :
