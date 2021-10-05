287c87aaa081a868559539ae0bb5f6ff95cb8b78
BastienChicot <88327915+BastienChicot@users.noreply.github.com>
2021-10-05 22:10:04 +0200

    mais pourquoi

--- Services/stats_joueurs.py
+++ Services/stats_joueurs.py
@@ -67,10 +67,10 @@ def stat_20matchs_splits(full_name):
            
     #df_splits_career = df_splits_career.groupby(axis = 1, level = 1).sum()
     #df_splits_career = df_splits_career.set_index(['Value'])
-    #df_splits_career = df_splits_career.loc[df_splits_career.index.isin##(['Home','Road','Monday','Tuesday','Wednesday','Thursday','Friday',
+    #df_splits_career = df_splits_career.loc[df_splits_career.index.isin###(['Home','Road','Monday','Tuesday','Wednesday','Thursday','Friday',
                                                                              #'Saturday','Sunday','0 Days','1 Day','2 Days','3+ Days','Eastern',
                                                                              #'Western','Atlantic','Central','Northwest','Southeast','Southwest',
-                                                                             'Pacific'])] 
+#                                                                             'Pacific'])] 
 #    df_splits_career.reset_index(level=0, inplace=True)
 
     return(url)
